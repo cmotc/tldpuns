f9cd858a46240a5777ef73d608937b97a0958ee0
cmotc <cmotc@openmailbox.org>
2015-09-03 15:23:12 -0400

    program to bruteforce potential top-level domains from wordlists and country codes

--- compare.sh
+++ compare.sh
@@ -1,18 +1,12 @@
 #! /bin/bash
-while read p; do
-	cat words.txt | grep $p > intermediate/$p.txt
-	for f in intermediate/*.txt; do
-		while read q; do
-			size=${#q}
-			second=` expr $size - 2 `
-#			echo $size $q $second $p
-#			echo ` expr "$q" : "$p" `
-			third=` expr "$q" : "$p" `
-			if [ $second == $third ]; then
-				echo $q >> build/$p.txt
-				echo $size $q $second "==" $third $p
-			fi
-#			echo ${q:$second:$p} #>> build/$p.txt
-		done <$f
-	done
-done <cctld.txt
+rm build -rf
+mkdir build
+cp words.txt words.work.txt
+while read q; do
+	while read p; do
+		if [ ${q: -2} == $p ]; then
+			echo $q >> build/$p.list
+			echo $q > list.last
+		fi
+	done <cctld.txt
+done <words.txt
